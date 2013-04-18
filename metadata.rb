b16db7723e9c9dc873710462eee8b488eef58dce
Scott Bader <sb@scottbader.org>
2013-04-18 13:57:24 -0400

    initial

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Go Try It On, Inc."
+maintainer_email "scott@gotryiton.com"
+license          "All rights reserved"
+description      "Installs/Configures percona-tools"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.3"
+
+depends           "apt"
+depends           "dotdeb"
